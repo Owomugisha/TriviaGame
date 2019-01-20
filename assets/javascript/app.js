6802eab0f3cf680c93a33c7e3b280ca73bc88fdf
Owomugisha <brinnetbirungi@yahoo.com>
2019-01-19 22:02:04 -0500

    add an image logo, created start, restart, and done btn, add correct and incorrect answers, Time remaining, on the app.js i created a function to hide finalresult,question and done when the game starts, created the questionArray, used for each loop to loop through each qtn and put the answers in a radio btn, created a userselection to check if correct answer is selected add on correctanswercount or else add to incorrectanswercounter, then created a timer for 20 seconds, also add a donebtn to stop and give you results

---
+++ assets/javascript/app.js
@@ -0,0 +1,91 @@
+$(document).ready(function () {
+    // console.log ("hello");
+    //hide when the game starts
+    $("#finalResult, #question, #donebtn").hide()
+    
+    var questionsArray = [
+
+        ["What color shoes does Mickey Mouse traditionally wear?", "Yellow", ["Red", "Blue", "Yellow", "Black"]],
+        ["What were Mickey's first spoken words?", "Hot Dogs!", ["Look out!", "Hot Dogs!", "Wanna Dance!", "Golly Gee Whillakers!"]],
+        ["In what year did Mickey Mouse make his first comic strip appearance?", "1930", ["1930", "1934", "1942", "1946"]],
+        ["What is the name of Mickey's sister?", "Amelia", ["Amelia", "Bonnie", "Carla", "Doreen"]]
+
+    ];
+
+    questionsArray.forEach((element, i) => {
+        //  console.log(element)
+        $("#question").append("<p>" + element[0] + "</p>")
+        element[2].forEach(answerChoice => {
+            $("#question").append(
+                //    "<input type = 'radio' name ='qtn'"+i+">" + answerChoice + ""
+                `<input class='userSelection' type='radio' correctAnswer='${element[1]}' name='qtn${i}' value='${answerChoice}'> ${answerChoice}`
+            )
+            //console.log(answerChoice)
+        });
+    });
+    var correctAnswerCounter = 0;
+    var incorrectAnswerCounter = 0;
+    $(".userSelection").on("click", function () {
+        //console.log("hi");   
+        var correctAnswer = $(this).attr('correctAnswer');
+        var userSelection = $(this).val()
+        console.log(correctAnswer, userSelection);
+        if (correctAnswer === userSelection) {
+            correctAnswerCounter++;
+            console.log("correctAnswerCounter", correctAnswerCounter);
+        } else {
+            incorrectAnswerCounter++;
+            console.log("incorrectAnswerCounter", incorrectAnswerCounter);
+        }
+    });
+    var timer;
+    var counter = 20;
+    $("#startbtn").on("click", function () {
+        //console.log("birungi");
+        $("#startbtn, #logo").hide();
+        //show qtns
+        $("#question, #donebtn").show();
+        
+        timer = setInterval(() => {
+            counter--;
+            if (counter >= 0) { 
+                $("#countDown").text(counter);
+             }
+             else{
+                 $("#question, #donebtn").hide();
+                 $("#finalResult").show();
+                 $("#correctAnswer").text(correctAnswerCounter);
+                 $("#incorrectAnswer").text(incorrectAnswerCounter);
+                 
+             }
+
+        }, 1000);
+    })
+
+$("#donebtn").on("click", function () {
+    clearInterval(timer);
+    counter = 20;
+    $("#question").hide();
+    $("#finalResult").show();
+    $("#correctAnswer").text(correctAnswerCounter);
+    $("#incorrectAnswer").text(incorrectAnswerCounter);
+})
+$("#restartbtn").on("click",function(){
+    
+    $("#correctAnswer, #incorrectAnswer").hide();
+        
+        $("#question, #donebtn").show();
+})
+
+    //    for(var i =0; i<questionsArray.length; i++){
+    //        var e = questionsArray[i];
+    //        //console.log(e)
+    //        for (let j = 0; j < e[2].length; i++) {
+    //            //var x = e[j]
+    //            //const element = questionsArray[i][j];
+    //            //console.log(questionsArray[i][0])
+    //            //console.log(x);
+    //        }
+    //    }
+})
+
